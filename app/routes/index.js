ae9b337207bfeb0e269004b1496e1555a310cec9
Victor Martins <victor@avanz.com.br>
2016-08-02 17:37:16 -0300

    remove console.log

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,8 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (res, req, next) {
-  req.json({message: 'welcome'})
-  console.log(req.sessionID);
+  req.json({message: 'welcome'});
 });
 
 module.exports = router;
